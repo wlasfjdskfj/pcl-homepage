e6361e4227439456fc20b3d26017b129d6cf0c1c
wlasfjdskfj <wlasfjdskfj@users.noreply.github.com>
2026-09-18 18:30:43 +0800

    favicon加缓存破坏参数强制刷新

--- functions/admin.js
+++ functions/admin.js
@@ -926,7 +926,7 @@ export async function onRequest(context) {
 <script>${THEME_SCRIPT}</script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="icon" type="image/png" href="/favicon.png">
+<link rel="icon" type="image/png" href="/favicon.png?v=2">
 <title>访问统计</title>
 <style>
   ${THEME_CSS}
